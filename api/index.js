e78677ab7d1450a15d68de336ae818b564252ad5
Dimitri del Marmol <dimitri.delmarmol@gmail.com>
2013-09-05 18:41:10 +0200

    Cleanup

--- api/index.js
+++ api/index.js
@@ -1,22 +1,22 @@
 var suggest = require('./suggest');
 var recommend = require('./recommend');
 
+function render (res) {
+  return function renderJSON (data) {
+    res.end(JSON.stringify(data, null, 2));
+  };
+}
+
 module.exports = {
 
   routes: function configureRoutes (app, middleware) {
     app.get('/suggest/:keyword', middleware.json, function (req, res) {
-      suggest(req.params.keyword, function (suggestions) {
-        res.end(JSON.stringify(suggestions, null, 2));
-      });
-
+      suggest(req.params.keyword, render(res));
     });
     app.get('/recommend/:keyword', middleware.json, function (req, res) {
-      recommend(req.params.keyword, function (recommendations) {
-        res.end(JSON.stringify(recommendations, null, 2));
-      });
+      recommend(req.params.keyword, render(res));
     });
   }
 
 };
-
 
