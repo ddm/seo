5f76e036a12f5b49ec0ddb7c559668691c873af2
Dimitri del Marmol <dimitri.delmarmol@gmail.com>
2013-10-18 18:29:29 +0200

    UTF-8 support

--- api/suggest.js
+++ api/suggest.js
@@ -3,8 +3,14 @@ var request = require('request');
 var parseString = require('xml2js').parseString;
 
 function getGoogleSuggestions (keyword, callback) {
-  var url = "http://google.com/complete/search?output=toolbar&q=" + encodeURIComponent(keyword);
-  request(url, function(error, response, xmlResponse) {
+  var url = "http://google.com/complete/search?ie=utf8&oe=utf8&output=toolbar&q=" + encodeURIComponent(keyword);
+  request({
+      url: url,
+      encoding: 'utf8',
+      headers: {
+          "Accept-Charset": "utf-8"
+      }
+  }, function(error, response, xmlResponse) {
     parseString(xmlResponse, function (err, jsonResponse) {
       var suggestions = _.uniq(_.filter(_.map(jsonResponse.toplevel.CompleteSuggestion, function (item) {
         var suggestion = item.suggestion[0].$.data;
--- middleware/json.js
+++ middleware/json.js
@@ -1,5 +1,5 @@
 module.exports = function jsonContentType (req, res, next) {
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
   next();
 };
 
